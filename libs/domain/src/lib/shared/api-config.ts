daabadf832f9bc016db9cce067193da83dc505c0
guilopesfeitosa <guilopesfeitosa@gmail.com>
2024-01-18 14:15:52 -0300

    feat: atualiza api-config novamente

--- libs/domain/src/lib/shared/api-config.ts
+++ libs/domain/src/lib/shared/api-config.ts
@@ -1,5 +1,5 @@
 class ApiConfig {
-  private static apiUrl: string = process.env.API_URL ? process.env.API_URL as string : 'http://localhost:3000/api';
+  private static apiUrl: string = process.env['API_URL'] ? process.env['API_URL'] as string : 'http://localhost:3000/api';
 
   static getApiUrl(): string | undefined {
     return ApiConfig.apiUrl;
