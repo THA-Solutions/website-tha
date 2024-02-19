f0d917564c41ad47093cc6702ca4d845db969081
Gean <tecnologia@thasolutions.com.br>
2024-02-19 10:52:48 -0300

    .

--- apps/back/api/src/mail/mail.module.ts
+++ apps/back/api/src/mail/mail.module.ts
@@ -9,6 +9,7 @@ import { MailerModule } from '@nestjs-modules/mailer';
       transport: {
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
+        secure: true,
         auth: {
           user: process.env.MAIL_USER,
           pass: process.env.MAIL_PASSWORD
